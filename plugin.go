3b566fe735e0259ed5c094fcf00d323f5cf2399e
David Ferlay <dferlay@skilld.cloud>
2025-02-14 17:39:40 +0100

    Removed keyring from plugin.go and updated package name

--- plugin.go
+++ plugin.go
@@ -1,11 +1,9 @@
-// Package launchrctlexample implements a example launchr plugin
-package launchrctlexample
+// Package example implements an example launchr plugin
+package example
 
 import (
 	"context"
 	_ "embed"
-
-	"github.com/launchrctl/keyring"
 
 	"github.com/launchrctl/launchr"
 	"github.com/launchrctl/launchr/pkg/action"
@@ -19,36 +17,23 @@ func init() {
 }
 
 // Plugin is [launchr.Plugin] providing example action.
-type Plugin struct {
-	k keyring.Keyring
-}
+type Plugin struct{}
 
 // PluginInfo implements [launchr.Plugin] interface.
 func (p *Plugin) PluginInfo() launchr.PluginInfo {
-	return launchr.PluginInfo{
-		Weight: 10,
-	}
-}
-
-// OnAppInit implements [launchr.OnAppInitPlugin] interface.
-func (p *Plugin) OnAppInit(app launchr.App) error {
-	app.GetService(&p.k)
-	return nil
+	return launchr.PluginInfo{}
 }
 
 // DiscoverActions implements [launchr.ActionDiscoveryPlugin] interface.
 func (p *Plugin) DiscoverActions(_ context.Context) ([]*action.Action, error) {
 	a := action.NewFromYAML("example", actionYaml)
-	a.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
-		input := a.Input()
-		username := input.Opt("username").(string)
-		password := input.Opt("password").(string)
-		return example(username, password, p.k)
+	a.SetRuntime(action.NewFnRuntime(func(_ context.Context, _ *action.Action) error {
+		return example()
 	}))
 	return []*action.Action{a}, nil
 }
 
-func example(username, password string, k keyring.Keyring) error {
+func example() error {
 	// Code to execute goes here
 	launchr.Term().Println("Hello world")
 
